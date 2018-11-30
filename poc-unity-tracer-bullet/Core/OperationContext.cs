55766e21a6cfdd1368f30f7cb184634d73d4b7d4
Niels Abels <n.abels@coolblue.nl>
2018-11-30 22:48:40 +0100

    Adds OperationContext to share localised state

---
+++ poc-unity-tracer-bullet/Core/OperationContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poc_synthetic_transaction.Core
+{
+    public class OperationContext<T>
+    {
+        private static T _current;
+
+        public static T GetCurrent() => _current;
+
+        public static IDisposable Start(T @object)
+        {
+            _current = @object;
+
+            return new Disposer();
+        }
+
+        public class Disposer : IDisposable
+        {
+            public void Dispose()
+            {
+                _current = default(T);
+            }
+        }
+    }
+}
